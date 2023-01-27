19648bd333cff006edddc977019e8b3447c5caf6
FabrizioMisseri <misserifabrizio@gmail.com>
2023-01-27 19:28:41 +0100

    add: axios call => posts-list

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,59 @@
 <script>
+import { store } from '../store';
+import axios from 'axios';
+
 export default {
     name: 'HomePage',
+
+    data() {
+        return {
+            store,
+            arrayPosts: [],
+            successPost: '',
+        }
+    },
+
+    created() {
+        this.axiosCall();
+    },
+
+    methods: {
+        axiosCall() {
+            axios.get(`${this.store.baseApiUrl}posts`).then((resp) => {
+                this.successPost = resp.data.success;
+                this.arrayPosts = resp.data.response;
+                //console.log(this.successPost);
+                console.log(this.arrayPosts);
+            });
+        }
+    }
 }
 </script>
 
 <template>
-
+    <div class="container py-5">
+        <div class="row">
+            <div class="card mb-4 py-3" v-for="(post, index) in arrayPosts" :key="index">
+                <ul>
+                    <li>
+                        <h4>{{ post.name }}</h4>
+                    </li>
+                    <li>
+                        <h5>{{ post.id }}</h5>
+                    </li>
+                    <li>
+                        <p>
+                            {{ post.text }}
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss">
-
+ul {
+    list-style: none;
+}
 </style>
